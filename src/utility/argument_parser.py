22ea7ec4c8c11e3fac90ec86bbe590f0b273045c
dmanns <d.manns@sva.de>
2022-12-21 17:15:03 +0100

    removed number of repetitions from arguments (already contained in cross validation)

--- src/utility/argument_parser.py
+++ src/utility/argument_parser.py
@@ -11,6 +11,5 @@ def parse() -> argparse.Namespace:
     arg = parser.add_argument
     arg("-m", "--mode", type=int, default=0, help="Mode of project (0 = train, 1 = postprocessing, 2 = inference)")
     arg("-e", "--exp_name", type=str, help="Experiment name stored in ./experiments as .yaml file", required=True)
-    arg("-r", "--repetitions", type=int, default=2, help="Number of repetitions for each experiment")
 
     return parser.parse_args()
