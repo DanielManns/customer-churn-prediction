b1a616e3cdd0f88dec01b10187a0d201cfdfd642
dmanns <d.manns@sva.de>
2023-01-11 16:03:03 +0100

    Fixed programmatic start of FastAPI

--- backend/src/backend/api.py
+++ backend/src/backend/api.py
@@ -7,11 +7,12 @@ conf = BackendConfig.from_yaml()
 
 
 def start_api():
-    uvicorn.run(__file__ + ":" + app.__str__(), reload=conf.reload, host=conf.host, port=conf.port, log_level="info")
+    uvicorn.run(__name__ + ":app", reload=conf.reload, host=conf.host, port=conf.port, log_level="info")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 
 
