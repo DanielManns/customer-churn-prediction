d4d8ca87902c648655d2cf92c4ba4234df11a81f
dmanns <d.manns@sva.de>
2023-01-11 16:34:38 +0100

    Added todos

--- frontend/src/frontend/main.py
+++ frontend/src/frontend/main.py
@@ -4,6 +4,15 @@ from frontend.src.frontend.config import FrontendConfig
 
 conf = FrontendConfig.from_yaml()
 
+# TODO:
+#  1. Configure experiment in first tab
+#  2. Send experiment data to backend
+#  3. Configure example row in second tab
+#  4. Send example row to backend for prediction
+#  5. Display prediction
+#  6. Obtain clf explanation from backend (feature importance)
+#  7. Display clf explanation with plots in third tab
+
 
 def run_gui(exp_config):
     # df, _ = get_preprocessed_dataset("mixed", exp_config["features"]["is_subset"], mode="test")
